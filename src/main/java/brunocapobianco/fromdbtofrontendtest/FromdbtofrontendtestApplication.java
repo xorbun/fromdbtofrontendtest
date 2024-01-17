a3b988cca758d2d6e0602ad91dff911a98feae46
Bruno Capobianco <bruno.capobianco@outlook.it>
2024-01-17 14:02:09 +0100

    first commit
    first commit

---
+++ src/main/java/brunocapobianco/fromdbtofrontendtest/FromdbtofrontendtestApplication.java
@@ -0,0 +1,13 @@
+package brunocapobianco.fromdbtofrontendtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FromdbtofrontendtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FromdbtofrontendtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/brunocapobianco/fromdbtofrontendtest/FromdbtofrontendtestApplicationTests.java
@@ -0,0 +1,13 @@
+package brunocapobianco.fromdbtofrontendtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FromdbtofrontendtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
